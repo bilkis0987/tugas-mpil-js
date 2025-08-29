125c7ab4410ffdaeddb6487a62921e027e701208
bilpas <angsianosaputra@gmail.com>
2025-08-29 21:22:38 +0700

    menambahkan file
    tugas mpil menghitung jumlah huruf dan hasilnya di kalikan 5

---
+++ tugas.js
@@ -0,0 +1,8 @@
+let nama = "bilkis";
+let dikalikan = 5;
+
+console.log("nama saya " + nama);
+console.log("jumlah huruf nama saya = " + nama.length);
+console.log(
+  "hasil jumlah huruf nama saya di kalikan 5 = " + nama.length * dikalikan
+);
